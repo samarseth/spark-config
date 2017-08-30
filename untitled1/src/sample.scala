9268efed8bb888cd62cfd9c3a68b59bb0b855606
Samar Seth <samarseth@aol.com>
2017-08-30 17:37:41 +0530

    Added Comments

--- untitled1/src/sample.scala
+++ untitled1/src/sample.scala
@@ -1,5 +1,4 @@
 import java.sql.{DriverManager, ResultSet}
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.JdbcRDD
@@ -9,7 +8,7 @@ import org.apache.spark.sql._
 object sample {
   def main(args: Array[String]): Unit = {
 
-    // Required setup
+    // Required setup to create Spark RDDs
 
     val sparkConf = new SparkConf()
     val cnf = sparkConf.setMaster("local[*]")
@@ -25,49 +24,51 @@ object sample {
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val df2 = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "CPRBody").load("D:\\Users\\sseth\\Desktop\\CPR905.xml")
+
+    // The filtered RDD has selected 3 columns from the fd2 dataframe
     val filteredDF = df2.select("ISIN", "RptDt", "BusDt")
     filteredDF.show()
 
-    // df2.select("ISIN").map(x => x.toString().substring(0,3)).foreach(println)
+    df2.select("ISIN").map(x => x.toString().substring(0,3)).foreach(println)
 
     df2.select("ISIN").map(x => x.toString().length == 14).foreach(println)
-   val lengthRec = filteredDF.map(x => records(x.getAs[String]("ISIN"), x.getAs[String]("RptDt"), x.getAs[String]("BusDt")))
-   .filter(rec => rec.BusDt > "2014-05-06")
-   .filter(rec => rec.ISIN.toString.length == 12)
+    val lengthRec = filteredDF.map(x => records(x.getAs[String]("ISIN"), x.getAs[String]("RptDt"), x.getAs[String]("BusDt")))
+    .filter(rec => rec.BusDt > "2014-05-06")
+    .filter(rec => rec.ISIN.toString.length == 12)
 
     
     println("It starts here")
     lengthRec.foreach(x => println(x))
     println("It ends here")
 
-   val finalDF = sqlContext.createDataFrame(lengthRec)
+    val finalDF = sqlContext.createDataFrame(lengthRec)
     println("The final dataframe starts here")
     finalDF.show()
     println("The final dataframe ends here")
 
 
     // Table extracted from MySQL database is converted into a dataframe
-    val jdbcDF = sqlContext.read.format("jdbc").options(
-        Map("url" -> "jdbc:mysql://localhost/test?user=root&password=mysql",
-        "dbtable" -> "test.cprValid4")).load()
-        jdbcDF.show()
+     val jdbcDF = sqlContext.read.format("jdbc").options(
+       Map("url" -> "jdbc:mysql://localhost/test?user=root&password=mysql",
+       "dbtable" -> "test.cprValid4")).load()
+       jdbcDF.show()
 
   }
 
-  def createConnection() = {
-    Class.forName("com.mysql.jdbc.Driver").newInstance();
-    DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=mysql");
-  }
+   def createConnection() = {
+   Class.forName("com.mysql.jdbc.Driver").newInstance();
+   DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=mysql");
+   }
 
-  def extractValues(r: ResultSet) = {
-    (r.getInt(1), r.getString(2))
-  }
+   def extractValues(r: ResultSet) = {
+   (r.getInt(1), r.getString(2))
+   }
 
-  def filterByLength(l: String): Boolean = {
-    if (l.length == 14) true else false
-  }
+   def filterByLength(l: String): Boolean = {
+   if (l.length == 14) true else false
+   }
 
 }
-
+// created a case class to store the elements of the dataframe
 case class records(ISIN: String, RptDt: String, BusDt: String)
 
