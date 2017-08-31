1ce1e74950cf95bf30310d4756693ba71b0b60c8
Samar Seth <samarseth@aol.com>
2017-08-31 14:27:59 +0530

    New Commit

--- untitled1/src/sample.scala
+++ untitled1/src/sample.scala
@@ -14,7 +14,6 @@ object sample {
     val cnf = sparkConf.setMaster("local[*]")
     sparkConf.setAppName("Sample Tests")
     val sc = new SparkContext(cnf)
-
     // Textfile from Desktop is converted into an RDD
 
     val rdd = sc.textFile("D:\\Users\\sseth\\Desktop\\Book1.csv")
@@ -32,19 +31,31 @@ object sample {
     df2.select("ISIN").map(x => x.toString().substring(0,3)).foreach(println)
 
     df2.select("ISIN").map(x => x.toString().length == 14).foreach(println)
+
+    // An RDD is created and filter criteria are applied to the newly created RDD
+
     val lengthRec = filteredDF.map(x => records(x.getAs[String]("ISIN"), x.getAs[String]("RptDt"), x.getAs[String]("BusDt")))
     .filter(rec => rec.BusDt > "2014-05-06")
     .filter(rec => rec.ISIN.toString.length == 12)
 
-    
-    println("It starts here")
+    println("The RDD starts here")
     lengthRec.foreach(x => println(x))
-    println("It ends here")
+    withCountry.foreach(x =>println(x))
+    println("The RDD ends here")
 
     val finalDF = sqlContext.createDataFrame(lengthRec)
+
+    // A new column is added to this dataframe
+    val modDF = finalDF.withColumn("Country Code", finalDF("ISIN").substr(0,2))
+
     println("The final dataframe starts here")
     finalDF.show()
     println("The final dataframe ends here")
+
+
+    println("The dataframe with an added column starts here")
+    modDF.show()
+    println("The dataframe with an added column ends here")
 
 
     // Table extracted from MySQL database is converted into a dataframe
